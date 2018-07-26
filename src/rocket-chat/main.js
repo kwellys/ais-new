892ae51f5d1ac48572228db2ae4bb5076d35560d
Eugene <eugene@wellyes.ru>
2018-07-26 13:18:38 +0300

    - Add widget function

--- src/rocket-chat/main.js
+++ src/rocket-chat/main.js
@@ -1,71 +1,68 @@
 (function() {
+  // console.log('Widget was loaded!');
+  var script = document.createElement("script");
+  script.src = "https://code.jquery.com/jquery-3.3.1.min.js";
+  script.onload = function() {};
 
-    // console.log('Widget was loaded!');
-    var script = document.createElement("script");
-    script.src = "https://code.jquery.com/jquery-3.3.1.min.js";
-    script.onload = function() {
-      
-    };
+  document.head.appendChild(script);
 
-    document.head.appendChild(script);
-
-    function widgetLoaded(func = f => f){
-      function check(){
-        return $('.title').length !== 0;
-      }
-
-      var timer = setInterval(() => {
-        var trigger = check();
-        if(trigger){
-          clearInterval(timer)
-          func()
-        }
-      })
+  function widgetLoaded(func = f => f) {
+    function check() {
+      return $(".title").length !== 0;
     }
-    
-    // $(document).ready(function() {
-      widgetLoaded();
-      console.log('Jquery was loaded')
-      var isOpen = false;
-      $(".livechat-room").addClass("widget-close");
 
-      function handleWidget() {
-        isOpen = !isOpen;
-        if (isOpen) {
-          $(".livechat-room").removeClass("widget-close");
-          $(".livechat-room").addClass("widget-open");
-        } else {
-          $(".livechat-room").removeClass("widget-open");
-          $(".livechat-room").addClass("widget-close");
-        }
+    var timer = setInterval(() => {
+      var trigger = check();
+      if (trigger) {
+        clearInterval(timer);
+        func();
       }
-      var title = $(".title");
-      var online = title.attr("style").indexOf("#C1272D") !== -1;
-      console.log(online);
-      title.on("click", function() {
-        handleWidget();
-      });
+    });
+  }
 
-      if (online) {
-        title.css({
-          background:
-            "linear-gradient(38.83deg, #66A6FF 30.26%, #6DFFD6 115.74%)"
-        });
-        console.log(online);
+  // $(document).ready(function() {
+  widgetLoaded(widget);
+  function widget() {
+    console.log("Jquery was loaded");
+    var isOpen = false;
+    $(".livechat-room").addClass("widget-close");
+
+    function handleWidget() {
+      isOpen = !isOpen;
+      if (isOpen) {
+        $(".livechat-room").removeClass("widget-close");
+        $(".livechat-room").addClass("widget-open");
       } else {
-        title.css({
-          background:
-            "linear-gradient(38.83deg, #97ABD4 27.05%, rgba(217, 230, 255, 0.69) 115.74%);"
-        });
+        $(".livechat-room").removeClass("widget-open");
+        $(".livechat-room").addClass("widget-close");
       }
-      // var max = $('.maximize')
-      // max.attr('viewBox', '0 0 28 28')
-      // max.attr('width', '28')
-      // max.attr('height', '28')
+    }
+    var title = $(".title");
+    var online = title.attr("style").indexOf("#C1272D") !== -1;
+    console.log(online);
+    title.on("click", function() {
+      handleWidget();
+    });
 
-      // max.children('path').attr('d', 'M18.6457 0H2.33071C1.03717 0 0 1.05 0 2.33333V16.3333C0 17.6167 1.03717 18.6667 2.33071 18.6667H18.6457C19.9276 18.6667 20.9764 17.6167 20.9764 16.3333V2.33333C20.9764 1.05 19.9392 0 18.6457 0ZM18.6457 16.3333H2.33071V4.66667H18.6457V16.3333Z')
-      // max.children('path').attr('fill', 'white')
-      // max.children('path').attr('transform', 'translate(3.49605 4.6665)')
-        // });
+    if (online) {
+      title.css({
+        background: "linear-gradient(38.83deg, #66A6FF 30.26%, #6DFFD6 115.74%)"
+      });
+      console.log(online);
+    } else {
+      title.css({
+        background:
+          "linear-gradient(38.83deg, #97ABD4 27.05%, rgba(217, 230, 255, 0.69) 115.74%);"
+      });
+    }
+    // var max = $('.maximize')
+    // max.attr('viewBox', '0 0 28 28')
+    // max.attr('width', '28')
+    // max.attr('height', '28')
 
+    // max.children('path').attr('d', 'M18.6457 0H2.33071C1.03717 0 0 1.05 0 2.33333V16.3333C0 17.6167 1.03717 18.6667 2.33071 18.6667H18.6457C19.9276 18.6667 20.9764 17.6167 20.9764 16.3333V2.33333C20.9764 1.05 19.9392 0 18.6457 0ZM18.6457 16.3333H2.33071V4.66667H18.6457V16.3333Z')
+    // max.children('path').attr('fill', 'white')
+    // max.children('path').attr('transform', 'translate(3.49605 4.6665)')
+    // });
+  }
 })();
