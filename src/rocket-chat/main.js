9324895113a8c56d93ce0c2e6751adc14195c1d2
Eugene <eugene@wellyes.ru>
2018-07-26 10:50:22 +0300

    - Add Jquery for rocketchat

--- src/rocket-chat/main.js
+++ src/rocket-chat/main.js
@@ -1,3 +1,7 @@
 window.onload = function () {
   console.log('Widget was loaded!');
+  var script = this.document.createElement('script');
+  script.src = "https://code.jquery.com/jquery-3.3.1.min.js";
+  script.onload = function () {console.log('Jquery was loaded')}
+  document.head.appendChild(script);
 };
