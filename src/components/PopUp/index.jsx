987b5a49e4747ee6f371ffe18c5ed4d1a1c45234
Kirill Petrov <k.petrov@wellyes.ru>
2018-08-03 13:17:15 +0300

    Form submission using formdata

--- src/components/PopUp/index.jsx
+++ src/components/PopUp/index.jsx
@@ -4,24 +4,6 @@ import React, { Component } from "react";
 
 import style from "./style";
 
-function encode(data) {
-  const formData = new FormData();
-  const res = Object.keys(data).reduce(((prev, val) => {
-    return {
-      ...prev,
-      [val] : data[val].value,
-    }
-  }),{});
-  res['form-name'] = data['form-name'];
-  res['file'] = data['file'];
-
-  for (const key of Object.keys(res)) {
-    formData.append(key, res[key]);
-  }
-
-  return formData;
-}
-
 class PopUp extends Component {
   constructor(props) {
     super(props);
@@ -35,8 +17,7 @@ class PopUp extends Component {
       form: {
         name: { invalid: false, value: "", required: true },
         email: { invalid: false, value: "", required: true },
-        phone: { invalid: false, value: "" },
-        file: null
+        phone: { invalid: false, value: "" }
       },
       loading: false,
       success: false,
@@ -101,9 +82,7 @@ class PopUp extends Component {
   handleChange(name, values) {
     const { form } = this.state;
     const newForm = Object.keys(form).reduce((total, key) => {
-      if (key === 'file') {
-        total[key] = values;
-      } else if (key === name) {
+      if (key === name) {
         total[key] = { ...values };
       } else {
         total[key] = { ...form[key] };
@@ -116,17 +95,10 @@ class PopUp extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
+
     if (this.isFormValid()) {
       const form = event.target;
-      //form.submit();
-      fetch("/", {
-        method: "POST",
-        body: encode({
-          "form-name": form.getAttribute("name"),
-          ...this.state.form
-        })
-      })
-        .then(console.log('done'));
+      form.submit();
     } else {
       this.setErrorForFields();
     }
