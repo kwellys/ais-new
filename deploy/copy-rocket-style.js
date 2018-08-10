db4a317648767370b183a56f5e1535ac3bae1c3d
Eugene <eugene@wellyes.ru>
2018-08-10 10:42:01 +0300

    - Add rocket image in deploy script

--- deploy/copy-rocket-style.js
+++ deploy/copy-rocket-style.js
@@ -3,5 +3,6 @@ const cpFile = require('cp-file');
 (async () => {
   await cpFile('src/rocket-chat/styles.css', 'public/rocket-chat/styles.css');
   await cpFile('src/rocket-chat/main.js', 'public/rocket-chat/main.js');
+  await cpFile('src/rocket-chat/images/icon.svg', 'public/rocket-chat/images/icon.svg');
   console.log('Files for rocket were copied');
 })();
