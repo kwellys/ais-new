a69fc7cb04be2b9fd303bae34908261c5a3cdbee
Eugene <eugene@wellyes.ru>
2018-08-13 17:24:46 +0300

    - Change button style

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -28,7 +28,7 @@ export default css`
     margin-right: 30px;
     border: none;
     box-shadow: 0px 2px 44px rgba(23, 128, 224, 0.271031);
-    ${flexAllAlign}
+    ${setStyle(flexAllAlign)}
     @media (max-width: 1300px) {
       padding: 0 14px;
       height: 27px;
